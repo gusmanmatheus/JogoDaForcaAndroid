613146ff9a2f108a7e50af5f308e025c71e35b7f
Magus <matheusenrik@hotmai.com>
2018-09-21 13:17:09 -0300

    Padrao camelCase inserido

--- app/src/main/java/atmconsutoria/mathe/jogodaforca/MainActivity.java
+++ app/src/main/java/atmconsutoria/mathe/jogodaforca/MainActivity.java
@@ -10,20 +10,19 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.List;
 import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
-    private int quantos_tracos;
-   private static int pontos = 0;
+    private int quantosTracos;
+    private static int pontos = 0;
 
     private String tentativa;
-    private  String[] traco_controler;
-    private  ArrayList<String> palavra_aleatoria;
+    private  String[] tracoControler;
+    private  ArrayList<String> palavraAleatoria;
     private static ImageView fotoForca;
-    private TextView letrasusadas;
-    private static TextView  tracos;
+    private TextView letrasUsadas;
+    private static TextView tracos;
     private TextView score;
-    private ListView lista_Letras;
+    private ListView listaLetras;
     private static int erro ;
     private ArrayList<String> letrasErradas = new ArrayList<String>();
     private boolean acabou=false;
@@ -56,20 +55,20 @@ public class MainActivity extends AppCompatActivity {
         palavras.add("lampada");
         //pegando palavra aleatoria
         aux = palavras.get((int) (Math.random() * palavras.size()));
-        quantos_tracos = aux.length();
-        palavra_aleatoria = new ArrayList<String>();
+        quantosTracos = aux.length();
+        palavraAleatoria = new ArrayList<String>();
 
         // palavra_aleatoria=palavras.get((int) (Math.random()*palavras.size()));
 
         ;
-        traco_controler = new String[quantos_tracos];
+        tracoControler = new String[quantosTracos];
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         fotoForca = findViewById(R.id.enforcado_Id);
         tracos = findViewById(R.id.palavr_Traco_Id);
         score = findViewById(R.id.score_Id);
-        letrasusadas=findViewById(R.id.letras_Usadas_Id);
-        lista_Letras = findViewById(R.id.lista_Botao_Id);
+        letrasUsadas=findViewById(R.id.letras_Usadas_Id);
+        listaLetras = findViewById(R.id.lista_Botao_Id);
         ArrayAdapter<String> adaptador = new ArrayAdapter<String>(
                 getApplicationContext(),
                 android.R.layout.simple_list_item_1,
@@ -77,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
                 letras
 
         );
-        lista_Letras.setAdapter(adaptador);
+        listaLetras.setAdapter(adaptador);
         String testee = "";
         for (int o = 0; aux.length() > o; o++) {
             testee += " - ";
@@ -87,18 +86,18 @@ public class MainActivity extends AppCompatActivity {
         //ficar cicando varias vezes
 
         for (int l = 0; aux.length() > l; l++) {
-            palavra_aleatoria.add(String.valueOf((aux.charAt(l))));
+            palavraAleatoria.add(String.valueOf((aux.charAt(l))));
         }
 
 
-        lista_Letras.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        listaLetras.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int posicao, long id) {
                 boolean ck = false;
 
 
                 for (int u = 0; letrasErradas.size() > u; u++) {
-//                    letrasusadas.setText(letrasusadas.getText()+" "+letrasErradas.get(u));
+//                    letrasUsadas.setText(letrasusadas.getText()+" "+letrasErradas.get(u));
 
                     if (letrasErradas.get(u) != letras[posicao]) {
 
@@ -118,15 +117,15 @@ public class MainActivity extends AppCompatActivity {
                         tentativa = letras[posicao];
                         Toast.makeText(getApplicationContext(), letras[posicao], Toast.LENGTH_SHORT).show();
 
-                        for (int i = 0; i < quantos_tracos; i++) {
-                            if (traco_controler == null || traco_controler.equals("")) {
-                                traco_controler[i] = " - ";
+                        for (int i = 0; i < quantosTracos; i++) {
+                            if (tracoControler == null || tracoControler.equals("")) {
+                                tracoControler[i] = " - ";
                             }
                         }
                         int controle = 0;
-                        for (int j = 0; j < quantos_tracos; j++) {
-                            if (palavra_aleatoria.get(j).toLowerCase().equals(tentativa.toLowerCase())) {
-                                traco_controler[j] = tentativa;
+                        for (int j = 0; j < quantosTracos; j++) {
+                            if (palavraAleatoria.get(j).toLowerCase().equals(tentativa.toLowerCase())) {
+                                tracoControler[j] = tentativa;
                                 controle++;
                             } else {
 
@@ -134,9 +133,9 @@ public class MainActivity extends AppCompatActivity {
                         }
                         int a = 0;
                         String teste = "";
-                        for (int j = 0; j < quantos_tracos; j++) {
-                            if (traco_controler[j] != null && !traco_controler[j].equals("")) {
-                                teste += traco_controler[j];
+                        for (int j = 0; j < quantosTracos; j++) {
+                            if (tracoControler[j] != null && !tracoControler[j].equals("")) {
+                                teste += tracoControler[j];
 
                             } else {
                                 teste += " - ";
@@ -162,11 +161,11 @@ public class MainActivity extends AppCompatActivity {
 
                             erro = 0;
                             aux = palavras.get((int) (Math.random() * palavras.size()));
-                            quantos_tracos = aux.length();
-                            traco_controler = new String[quantos_tracos];
-                            palavra_aleatoria = new ArrayList<String>();
+                            quantosTracos = aux.length();
+                            tracoControler = new String[quantosTracos];
+                            palavraAleatoria = new ArrayList<String>();
                             for (int l = 0; aux.length() > l; l++) {
-                                palavra_aleatoria.add(String.valueOf((aux.charAt(l))));
+                                palavraAleatoria.add(String.valueOf((aux.charAt(l))));
                             }
                             String testee = "";
                             for (int o = 0; aux.length() > o; o++) {
@@ -186,11 +185,11 @@ public class MainActivity extends AppCompatActivity {
                             letrasErradas = new ArrayList<String>();
 
                         }
-                        letrasusadas.setText("Letras usadas:");
+                        letrasUsadas.setText("Letras usadas:");
 
                         for (int u = 0; letrasErradas.size() > u; u++) {
-                            letrasusadas.setText(letrasusadas.getText()+" "+letrasErradas.get(u));
-                    }
+                            letrasUsadas.setText(letrasUsadas.getText()+" "+letrasErradas.get(u));
+                        }
                     }
                 }
             }
@@ -214,8 +213,8 @@ public class MainActivity extends AppCompatActivity {
 
     }
     private static void restForca(ArrayList<String> palavras ){
-    int pol = palavras.size();
-     palavras.get((int) (Math.random()*pol+1));
+        int pol = palavras.size();
+        palavras.get((int) (Math.random()*pol+1));
 
 
 
@@ -237,8 +236,8 @@ public class MainActivity extends AppCompatActivity {
 
         }
         if (erro==4){
-           fotoForca.setImageResource(R.drawable.forca4erro);
-                  }
+            fotoForca.setImageResource(R.drawable.forca4erro);
+        }
         if (erro==5){
             fotoForca.setImageResource(R.drawable.forca5erros);
 
